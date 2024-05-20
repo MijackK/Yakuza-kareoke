0c100c6143ec54a74506ae81dbd3be506fe607a3
Mijack <61523653+MijackK@users.noreply.github.com>
2024-05-20 17:28:49 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
         type: "asset/resource",
       },
       {
